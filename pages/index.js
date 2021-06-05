43d2dd3a29809f75c9d7b3464166999e387fad2f
Ricky Tanudjaja <rickytanudjaja95@gmail.com>
2021-06-05 12:46:24 +0700

    add sanity studio

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ data }) {
+  const recipes = data.recipes
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <a>{recipes[0].title}</a>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -66,4 +68,15 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: {
+        recipes: [{ title: "Pineapple Smoothie" }]
+      }
+    }
+  }
 }
