023bf2ae6a7d648064f9fe26e2025ba57219eb69
Ricky Tanudjaja <rickytanudjaja95@gmail.com>
2021-06-05 17:46:39 +0700

    create new connection to sanity && edit global.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Link from "next/link"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className="header">
+        <Link href="/">
+          <a>Home Kitchen</a>
+        </Link>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
--- studio/schemas/recipe.js
+++ studio/schemas/recipe.js
@@ -92,5 +92,13 @@ export default {
         }
       ],
     },
+    {
+      name: "instructions",
+      title: 'Instructions',
+      type: "array",
+      of: [{
+        type: "block"
+      }]
+    }
   ],
 }
